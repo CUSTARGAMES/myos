13a353e4b5dfc5dfa3a12991df028b2c7fd8a1c3
saadmann <mtaslimaroja@gmail.com>
2026-04-30 19:33:36 +0600

    Update boot.s

--- boot.s
+++ boot.s
@@ -2,39 +2,35 @@ section .multiboot
 align 4
 
 MAGIC    equ 0x1BADB002
-FLAGS    equ (1 << 0) | (1 << 1) | (1 << 2)
+FLAGS    equ (1 << 0) | (1 << 1)
 CHECKSUM equ -(MAGIC + FLAGS)
 
 dd MAGIC
 dd FLAGS
 dd CHECKSUM
-dd 0
-dd 0
-dd 0
-dd 0
-dd 0
-dd 1
-dd 640
-dd 480
-dd 8
 
 section .text
 global _start
 _start:
     mov esp, stack_top
-    push 0
-    popf
+    
+    ; Clear direction flag
+    cld
+    
+    ; Save multiboot info
     push ebx
     push eax
+    
     extern kernel_main
     call kernel_main
+    
     cli
-halt:
+.halt:
     hlt
-    jmp halt
+    jmp .halt
 
 section .bss
 align 16
 stack_bottom:
-    resb 16384
+    resb 32768
 stack_top:
